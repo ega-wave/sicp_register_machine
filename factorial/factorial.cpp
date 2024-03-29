67465f15d2e2b4d05003283ca54bf8c8a06e61a8
ega-wave <health@ega-wave.com>
2024-03-30 00:28:16 +0900

    add header

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -1,5 +1,33 @@
 
 /**
+factorial -- compute factorial(n) with "the register machine."
+
+"The register machine" is introduced in the Chapter 5 of the
+book "Structure and Interpretation of Computer Programs" by
+Harold Abelson and Gerald Jay Sussman with Julie Sussman.
+
+Copyright (C) 2024 Yoshitaka Egawa.
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Written by Yoshitaka Egawa.
+ */
+
+/**
  * (define (factorial n)
  *   (if (= n 1)
  *       1
